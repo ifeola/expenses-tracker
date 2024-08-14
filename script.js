310815e43ff740695797591b07fb47353ddbd2c3
Arogunmasa Abayomi <nomorelimit11@gmail.com>
2024-08-14 14:02:07 +0100

    Commiting all code

---
+++ script.js
@@ -0,0 +1,109 @@
+const tbody = document.querySelector("tbody");
+const form = document.querySelector("#tracker__form");
+
+class Expenses {
+  constructor(category, amount, date, id) {
+    this._category = category;
+    this._amount = amount;
+    this._date = date;
+    this._id = id;
+  }
+}
+
+class UI {
+  static showExpenses() {
+    const expenses = Store.getExpenses();
+
+    expenses.forEach((expense, index) => {
+      UI.addExpenses(expense, index);
+    });
+  }
+
+  static addExpenses(expense, index) {
+    // Adding Expenses
+    const tRow = document.createElement("tr");
+    tRow.innerHTML = `
+      <td id=${index}>0${index + 1}</td>
+      <td>${expense._category}</td>
+      <td>${expense._amount}</td>
+      <td>${expense._date}</td>
+      <td><button class='delete-exp'>Delete</button></td>
+    `;
+    tbody.appendChild(tRow);
+  }
+
+  static deleteExpenses(target) {
+    if (!target.classList.contains("delete-exp")) return;
+    else {
+      const row = target.parentElement.parentElement;
+      const rowParent = target.parentElement.parentElement.parentElement;
+      rowParent.removeChild(row);
+    }
+  }
+}
+
+class Store {
+  static getExpenses() {
+    let expensesArray;
+
+    if (localStorage.getItem("expensesArray") === null) {
+      expensesArray = [];
+    } else {
+      expensesArray = JSON.parse(localStorage.getItem("expensesArray"));
+    }
+    return expensesArray;
+  }
+
+  static addExpenses(expense) {
+    const expensesArray = Store.getExpenses();
+    expensesArray.push(expense);
+    localStorage.setItem("expensesArray", JSON.stringify(expensesArray));
+  }
+
+  static deleteExpenses(target) {
+    let targetValue =
+      target.parentElement.parentElement.firstElementChild.attributes.id.value;
+    const expensesArray = Store.getExpenses();
+
+    expensesArray.forEach((expense, index) => {
+      if (targetValue === index) {
+        expensesArray.splice(index, 1);
+      }
+    });
+    localStorage.setItem("expensesArray", JSON.stringify(expensesArray));
+  }
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  // Input values
+  let category = form.querySelector("#category");
+  let amount = form.querySelector("#amount");
+  let date = form.querySelector("#date");
+
+  let categoryValue = category.value;
+  let amountValue = amount.value;
+  let dateValue = date.value;
+
+  // Generate random ID anytime we submit
+  const id = Math.floor(Math.random() * 1000000000 + 1);
+
+  // Everytime we submit, we add new expenses to the UI
+  const expenses = new Expenses(categoryValue, amountValue, dateValue, id);
+  UI.addExpenses(expenses);
+  Store.addExpenses(expenses);
+
+  // Clear input fields
+  category.value = "";
+  amount.value = "";
+  date.value = "";
+});
+
+tbody.addEventListener("click", (e) => {
+  // Deleting expenses
+  UI.deleteExpenses(e.target);
+  Store.deleteExpenses(e.target);
+});
+
+document.addEventListener("DOMContentLoaded", UI.showExpenses);
